0200739f1aaff211f1f6a16d9870e5ecd0d1a712
unknown <dertefter@gmail.com>
2020-05-26 22:37:59 +0900

    0

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ def get_text_messages(message):
         bot.send_message(message.from_user.id, "Привет! Меня зовут Эмили. Просто отправь мне ссылку на приложение из Google Play!")
 
     if 'https' in message.text:
-        bot.send_message(message.from_user.id, "Ага, это ссылка")
+        bot.send_message(message.from_user.id, link_maker.create_link('https://') + message.text.partition('https://')[2])
+
 
 
 bot.polling(none_stop=True, interval=0)
